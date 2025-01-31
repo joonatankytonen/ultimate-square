d61759c84ca41d088c7a60b68dd74f533470f28f
hannatammela <hanna.tammela@seamk.fi>
2025-01-31 08:54:32 +0200

    aloitusrunko, ruutu piirtyy, palikka saatu liikumaan

--- src/main.py
+++ src/main.py
@@ -1,46 +1,40 @@
-# Tämä koodi on Esimerkki koodi Pygamen omilla dokumentti sivuilta!
-# https://www.pygame.org/docs/
-
-# Example file showing a circle moving on screen
 import pygame
+import random
 
-# pygame setup
+# Pygame asetukset
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ultimate Square")
 clock = pygame.time.Clock()
+
+# Pelaajan asetukset
+player_size = 40
+player_x, player_y = WIDTH//2, HEIGHT//2
+player_speed = 5
+
 running = True
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    screen.fill((255,255,255))   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
+    
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
+    if keys[pygame.K_LEFT]:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x += player_speed
+    if keys[pygame.K_UP]:
+        player_y -= player_speed
+    if keys[pygame.K_DOWN]:
+        player_y += player_speed
+    
+    
+    # Piirrä pelaaja
+    pygame.draw.rect(screen, (255,0,0), (player_x, player_y, player_size, player_size))
+    
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
+    clock.tick(30)
+    
 pygame.quit()
