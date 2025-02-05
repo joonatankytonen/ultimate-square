683a955e463cc0391e59232c308544b0439288ed
Joonatan Kytönen <joonatan.kytonen@gmail.com>
2025-02-05 20:01:41 +0200

    Päivitetty pelaajan liikkumista:
    	Pelin alussa pelaajalla ei ole liikettä, mutta kun jotain
    	nuolinappia painaan niin pelaaja lähtee liikkeelle.
    
    	Pelaaja liikkuu koko ajan, eikä voi pysähtyä. Nuolinäppäimillä
    	pystyy vaihtaa pelaajan liikkumisen suuntaa.

--- src/main.py
+++ src/main.py
@@ -19,8 +19,10 @@ text_right = font.render("Score", True, (0, 0, 0))
 
 # Pelaajan asetukset
 player_size = 40
-player_x, player_y = WIDTH//2, HEIGHT//2
+player_xPosition, player_yPosition = WIDTH//2, HEIGHT//2 # Pelaajan alku position on pelin kentän keskellä
 player_speed = 5
+player_variable_x=0 # Tämä on pelaajan muuttuvat x ja y positiot jotka muuttavat oikeaa x ja y:tä
+player_variable_y=0 # Tämä on pelaajan muuttuvat x ja y positiot jotka muuttavat oikeaa x ja y:tä
 
 running = True
 game_over = False
@@ -32,22 +34,29 @@ while running:
             running = False
     
     if not game_over:
+        player_xPosition += player_variable_x # Päivitetään pelaajan sijaintia koko ajan, jotta pelaaja on koko ajan liikkeessä
+        player_yPosition += player_variable_y
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            player_x -= player_speed
+          player_variable_x = -player_speed # Aina nappia painaessa pelaaja vaihtaa suuntaa toiseen ja lopettaa vauhdin toisessa.
+          player_variable_y = 0
         if keys[pygame.K_RIGHT]:
-            player_x += player_speed
+          player_variable_x = player_speed
+          player_variable_y = 0
         if keys[pygame.K_UP]:
-            player_y -= player_speed
+          player_variable_x = 0
+          player_variable_y = -player_speed
         if keys[pygame.K_DOWN]:
-            player_y += player_speed
-        
+          player_variable_x = 0
+          player_variable_y = player_speed
+
         # Tarkistetaan, tuleeko törmäyksiä seinien kanssa, jos tulee, peli loppuu=True
-        if player_x <= 10 or player_x + player_size >= WIDTH - 10 or player_y <= 10 or player_y + player_size >= HEIGHT - 10:
+        if player_xPosition <= 10 or player_xPosition + player_size >= WIDTH - 10 or player_yPosition <= 10 or player_yPosition + player_size >= HEIGHT - 10:
             game_over = True
     
     # Piirrä pelaaja
-    pygame.draw.rect(screen, (255, 0, 0), (player_x, player_y, player_size, player_size))
+    pygame.draw.rect(screen, (255, 0, 0), (player_xPosition, player_yPosition, player_size, player_size))
     
     # Piirrä teksti ruudulle (esimerkiksi ylhäällä)
     screen.blit(text_left, ( 20, 20))  # Teksti keskitettynä
