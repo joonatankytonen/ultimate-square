f7676933b842e9fc1e5f42ff462324999549f14d
Joonatan Kytönen <joonatan.kytonen@gmail.com>
2025-02-17 19:53:32 +0200

    Luotu uusi metodi erilliselle ohjeruudulle. Yritän myös hiukan siistiä koodia ja laittaa myös pelin pyörimisen omaan metodiinsa, mutta tällä hetkellä siinä on hiukan bugeja.

--- src/main.py
+++ src/main.py
@@ -30,9 +30,7 @@ colors = {
 "Yellow": (255, 255, 0)
 }
 
-#pelin score 
-score = 0
-score_increment = 1
+
  
 # pelin aloitussivu
 def start_screen():
@@ -40,26 +38,6 @@ def start_screen():
   running = True
   while running:
     screen.fill((50, 50, 50)) # Taustaväri
-  
-    # Ohjeet vasemmassa yläkulmassa
-    text = """Move with arrow keys: up, down, left and right.
-    
-    Avoid obstacles and walls.
-    
-    Eat 10 foods to advance to the next level.
-    
-    The final stage is an endless mode!!!!!!"""
-    lines = text.split("\n")
-    x_margin = 10
-    y_start = 10
-    line_spacing = 30
-    
-    y = y_start
-    for line in lines:
-      rendered_text = font.render(line, True, (255, 255, 255))
-      x = x_margin
-      screen.blit(rendered_text, (x, y))
-      y += line_spacing
       
     # Värivalinnan otsikko ja nappulat keskellä
     color_text = font.render("Choose your player color:", True, (253, 253, 253))
@@ -88,7 +66,7 @@ def start_screen():
     # OHJEET-nappula
     ohjeet_button = pygame.Rect(WIDTH // 2 + 10, button_y, 120, 50)
     pygame.draw.rect(screen, (211, 211, 211), ohjeet_button, border_radius=15)
-    ohjeet_text = font.render("OHJEET", True, (0, 0, 0))
+    ohjeet_text = font.render("GUIDE", True, (0, 0, 0))
     text_x = ohjeet_button.x + (ohjeet_button.width - ohjeet_text.get_width()) // 2
     text_y = ohjeet_button.y + (ohjeet_button.height - ohjeet_text.get_height()) // 2
     screen.blit(ohjeet_text, (text_x, text_y))
@@ -109,100 +87,152 @@ def start_screen():
             print(f"Pelaajan väri valittu: {color_name}")
           button_x += 80
         if start_button.collidepoint(x, y):
-          running = False
+          game(player_color)
+        if ohjeet_button.collidepoint(x,y):
+          guide_screen()
+
+# ohjeet sivu
+def guide_screen():
+  running = True
+  while running:
+    screen.fill((50, 50, 50)) # Taustaväri
+  
+    # Ohjeet vasemmassa yläkulmassa
+    text = """Move with arrow keys: up, down, left and right.
     
-# Ruoka muuttujat
-foodColor=(254, 141, 77)
-foodSize=15
-ruokaOlemassa = 0
-new_food = None
+    Avoid obstacles and walls.
+    
+    Eat 10 foods to advance to the next level.
+    
+    The final stage is an endless mode!!!!!!"""
+    lines = text.split("\n")
+    x_margin = 10
+    y_start = 10
+    line_spacing = 30
+    
+    y = y_start
+    for line in lines:
+      rendered_text = font.render(line, True, (255, 255, 255))
+      x = x_margin
+      screen.blit(rendered_text, (x, y))
+      y += line_spacing
+      
+   
+    
+    # OHJEET-nappula
+    back_button = pygame.Rect(WIDTH // 2 + 10, 150, 120, 50)
+    pygame.draw.rect(screen, (211, 211, 211), back_button, border_radius=15)
+    back_text = font.render("BACK", True, (0, 0, 0))
+    text_x = back_button.x + (back_button.width - back_text.get_width()) // 2
+    text_y = back_button.y + (back_button.height - back_text.get_height()) // 2
+    screen.blit(back_text, (text_x, text_y))
+      
+    pygame.display.flip()
+      
+    for event in pygame.event.get():
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        x, y = event.pos
+        button_x = 10
+        button_y = 370
+      if back_button.collidepoint(x,y):
+        start_screen()
+
+# Peli
+def game(playerColor):
  
- 
+  # Ruoka muuttujat
+  foodColor=(254, 141, 77)
+  foodSize=15
+  ruokaOlemassa = 0
+  new_food = None
+  
+  # Pelaajan asetukset
+  player_size = 40
+  player_xPosition, player_yPosition = WIDTH//2, HEIGHT//2 # Pelaajan alku position on pelin kentän keskellä
+  player_speed = 5
+  player_variable_x=0 # Tämä on pelaajan muuttuvat x ja y positiot jotka muuttavat oikeaa x ja y:tä
+  player_variable_y=0 # Tämä on pelaajan muuttuvat x ja y positiot jotka muuttavat oikeaa x ja y:tä
+
+  #pelin score 
+  score = 0
+  score_increment = 1
+
+  running = True
+  game_over = False
+  
+  while running:
+    screen.fill((255, 255, 255)) # Täytetään näyttö valkoiseksi
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, WIDTH, HEIGHT), 10) # Piirretään reunat mustaksi
+    
+    # Piirrä pelaaja
+    player = pygame.draw.rect(screen, playerColor, (player_xPosition, player_yPosition, player_size, player_size))
+    
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+      
+    if not game_over:
+      player_xPosition += player_variable_x # Päivitetään pelaajan sijaintia koko ajan, jotta pelaaja on koko ajan liikkeessä
+      player_yPosition += player_variable_y
+    
+      keys = pygame.key.get_pressed()
+      if keys[pygame.K_LEFT]:
+        player_variable_x = -player_speed # Aina nappia painaessa pelaaja vaihtaa suuntaa toiseen ja lopettaa vauhdin toisessa.
+        player_variable_y = 0
+      if keys[pygame.K_RIGHT]:
+        player_variable_x = player_speed
+        player_variable_y = 0
+      if keys[pygame.K_UP]:
+        player_variable_x = 0
+        player_variable_y = -player_speed
+      if keys[pygame.K_DOWN]:
+        player_variable_x = 0
+        player_variable_y = player_speed
+      
+      # Jos ruokaa ei ole spawnaa yksi.
+      if ruokaOlemassa == 0:
+        x = random.randint(50, WIDTH-50)
+        y = random.randint(50, HEIGHT-50)
+        new_food = pygame.Rect(x,y,foodSize,foodSize)
+        ruokaOlemassa = 1
+      # Jos pelaaja osuu ruokaan poistaa se aikaisemman ruoan ja spawnaa uuden.
+      if player.colliderect(new_food):
+        print("syöty")
+        score += score_increment  # Lisää pistettä ruokaa syödessä
+        ruokaOlemassa = 0
+    
+      # Tarkistetaan, tuleeko törmäyksiä seinien kanssa, jos tulee, peli loppuu=True
+      if player_xPosition <= 10 or player_xPosition + player_size >= WIDTH - 10 or player_yPosition <= 10 or player_yPosition + player_size >= HEIGHT - 10:
+        game_over = True
+      
+      # Jos new_food True niin piirrä ruoka näytölle.
+      if new_food:
+        pygame.draw.rect(screen, foodColor, new_food)
+
+
+      # Tekstin renderöinti
+    text_left = font.render("Life", True, (0, 0, 0))
+    text_middle = font.render("Taso 1", True, (0, 0, 0))  # Väri (mustaa)
+    text_right = font.render(f"Score: {score}" , True, (0, 0, 0))
+    
+    
+    # Piirrä teksti ruudulle (esimerkiksi ylhäällä)
+    screen.blit(text_left, ( 20, 20)) # Teksti keskitettynä
+    screen.blit(text_middle, (WIDTH // 2 - text_middle.get_width() // 2, 20))
+    screen.blit(text_right, (WIDTH - text_right.get_width() - 20, 20))
+    
+    if game_over:
+      game_over_text = game_over_font.render("GAME OVER", True, (255, 0, 0)) # game over fontti
+      screen.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2))
+    
+    pygame.display.flip()
+    clock.tick(30)
 # kutsu aloitusnäyttöä
 start_screen()
  
 # pelaajan valitsema väri
 print("Pelaajan valitsema väri:", player_color)
- 
- 
-# Pelaajan asetukset
-player_size = 40
-player_xPosition, player_yPosition = WIDTH//2, HEIGHT//2 # Pelaajan alku position on pelin kentän keskellä
-player_speed = 5
-player_variable_x=0 # Tämä on pelaajan muuttuvat x ja y positiot jotka muuttavat oikeaa x ja y:tä
-player_variable_y=0 # Tämä on pelaajan muuttuvat x ja y positiot jotka muuttavat oikeaa x ja y:tä
- 
- 
- 
-running = True
-game_over = False
- 
-while running:
-  screen.fill((255, 255, 255)) # Täytetään näyttö valkoiseksi
-  pygame.draw.rect(screen, (0, 0, 0), (0, 0, WIDTH, HEIGHT), 10) # Piirretään reunat mustaksi
-  
-  # Piirrä pelaaja
-  player = pygame.draw.rect(screen, player_color, (player_xPosition, player_yPosition, player_size, player_size))
-  
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-    
-  if not game_over:
-    player_xPosition += player_variable_x # Päivitetään pelaajan sijaintia koko ajan, jotta pelaaja on koko ajan liikkeessä
-    player_yPosition += player_variable_y
-  
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-      player_variable_x = -player_speed # Aina nappia painaessa pelaaja vaihtaa suuntaa toiseen ja lopettaa vauhdin toisessa.
-      player_variable_y = 0
-    if keys[pygame.K_RIGHT]:
-      player_variable_x = player_speed
-      player_variable_y = 0
-    if keys[pygame.K_UP]:
-      player_variable_x = 0
-      player_variable_y = -player_speed
-    if keys[pygame.K_DOWN]:
-      player_variable_x = 0
-      player_variable_y = player_speed
-    
-    # Jos ruokaa ei ole spawnaa yksi.
-    if ruokaOlemassa == 0:
-      x = random.randint(50, WIDTH-50)
-      y = random.randint(50, HEIGHT-50)
-      new_food = pygame.Rect(x,y,foodSize,foodSize)
-      ruokaOlemassa = 1
-    # Jos pelaaja osuu ruokaan poistaa se aikaisemman ruoan ja spawnaa uuden.
-    if player.colliderect(new_food):
-      print("syöty")
-      score += score_increment  # Lisää pistettä ruokaa syödessä
-      ruokaOlemassa = 0
-  
-    # Tarkistetaan, tuleeko törmäyksiä seinien kanssa, jos tulee, peli loppuu=True
-    if player_xPosition <= 10 or player_xPosition + player_size >= WIDTH - 10 or player_yPosition <= 10 or player_yPosition + player_size >= HEIGHT - 10:
-      game_over = True
-    
-    # Jos new_food True niin piirrä ruoka näytölle.
-    if new_food:
-      pygame.draw.rect(screen, foodColor, new_food)
 
-
-    # Tekstin renderöinti
-  text_left = font.render("Life", True, (0, 0, 0))
-  text_middle = font.render("Taso 1", True, (0, 0, 0))  # Väri (mustaa)
-  text_right = font.render(f"Score: {score}" , True, (0, 0, 0))
-  
-  
-  # Piirrä teksti ruudulle (esimerkiksi ylhäällä)
-  screen.blit(text_left, ( 20, 20)) # Teksti keskitettynä
-  screen.blit(text_middle, (WIDTH // 2 - text_middle.get_width() // 2, 20))
-  screen.blit(text_right, (WIDTH - text_right.get_width() - 20, 20))
-  
-  if game_over:
-    game_over_text = game_over_font.render("GAME OVER", True, (255, 0, 0)) # game over fontti
-    screen.blit(game_over_text, (WIDTH // 2 - game_over_text.get_width() // 2, HEIGHT // 2))
-  
-  pygame.display.flip()
-  clock.tick(30)
-  
+# Sulje peli
 pygame.quit()
+
